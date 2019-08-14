e4c9cab519e6a6eca35d896b807de5e3f91ad137
Passos <Passos@passos>
2019-08-14 19:02:13 -0300

    Projeto gestão de festa

---
+++ src/main/java/com/algaworks/festa/model/Convidado.java
@@ -0,0 +1,74 @@
+package com.algaworks.festa.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Convidado implements Serializable {
+	private static final long serialVersionUID = -4320464083457448325L;
+	
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	private Long id;
+	
+	private String nome;
+	
+	private Integer quantidadeAcompanhantes;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Convidado other = (Convidado) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidadeAcompanhantes() {
+		return quantidadeAcompanhantes;
+	}
+
+	public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
+		this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+	}
+		
+}
